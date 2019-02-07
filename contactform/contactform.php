9b283f2fc0ad3fb4dc86f503d0f83911a0a2e495
MoustafaElhadary <MoustafaElhadary96@gmail.com>
2019-02-07 23:48:34 +0000

    r

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,34 +1,31 @@
 <?php
-require("/lib/sendgrid-php/sendgrid-php.php");
-
-$request_body = json_decode('{
-  "personalizations": [
-    {
-      "to": [
-        {
-          "email": "moustafaElhadary96@gmail.com"
-        }
-      ],
-      "subject": "Hello World from the SendGrid PHP Library!"
-    }
-  ],
-  "from": {
-    "email": "test@example.com"
-  },
-  "content": [
-    {
-      "type": "text/plain",
-      "value": "Hello, Email!"
-    }
-  ]
-}');
-
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
-
-$response = $sg->client->mail()->send()->post($request_body);
-echo $response->statusCode();
-echo $response->body();
-echo $response->headers();
-header("Location: index.php?mailsend")
+// You need to install the sendgrid client library so run: composer require sendgrid/sendgrid
+	require './lib/sendgrid-php/vendor/autoload.php';
+	// contains a variable called: $API_KEY that is the API Key.
+	// You need this API_KEY created on the Sendgrid website.
+	include_once('./credentials.php');
+	
+	$FROM_EMAIL = 'MoustafaElhadary96@gmail.com';
+	// they dont like when it comes from @gmail, prefers business emails
+	$TO_EMAIL = 'THE_PERSON_YOUR_WANT_TO_CONTACT';
+	// Try to be nice. Take a look at the anti spam laws. In most cases, you must
+	// have an unsubscribe. You also cannot be misleading.
+	$subject = "YOUR_SUBJECT";
+	$from = new SendGrid\Email(null, $FROM_EMAIL);
+	$to = new SendGrid\Email(null, $TO_EMAIL);
+	$htmlContent = '';
+	// Create Sendgrid content
+	$content = new SendGrid\Content("text/html",$htmlContent);
+	// Create a mail object
+	$mail = new SendGrid\Mail($from, $subject, $to, $content);
+	
+	$sg = new \SendGrid($API_KEY);
+	$response = $sg->client->mail()->send()->post($mail);
+			
+	if ($response->statusCode() == 202) {
+		// Successfully sent
+		echo 'done';
+	} else {
+		echo 'false';
+	}
 ?>
