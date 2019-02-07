934fb0cdef316f9d4a8de501f4617c8f3f937529
Moustafa <MoustafaElhadary96@gmail.com>
2019-02-07 02:31:35 -0500

    Added Pricing options and video modal

--- js/main.js
+++ js/main.js
@@ -135,6 +135,26 @@
     items: 1
   });
 
+
+  // Init Owl Carousel
+  $('.owl-carousel').owlCarousel({
+    items: 4,
+    autoplay: true,
+    loop: true,
+    margin: 30,
+    dots: true,
+    responsiveClass: true,
+    responsive: {
+
+      320: { items: 1},
+      480: { items: 2},
+      600: { items: 2},
+      767: { items: 3},
+      768: { items: 3},
+      992: { items: 4}
+    }
+  });
+
   // Modal video
   new ModalVideo('.js-modal-btn', {channel: 'youtube'});
 
