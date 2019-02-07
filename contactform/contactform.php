bf7b25e7215a99e525aa347d367b648b1b3af6fa
MoustafaElhadary <MoustafaElhadary96@gmail.com>
2019-02-07 23:07:56 +0000

    test2

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -88,27 +88,31 @@ require("../lib/sendgrid-php/sendgrid-php.php");
   // }
   
   $apiKey = getenv('SENDGRID_API_KEY');
-  $FROM_EMAIL = 'Moustafa@psu.edu';
-	// they dont like when it comes from @gmail, prefers business emails
-	$TO_EMAIL = 'THE_PERSON_YOUR_WANT_TO_CONTACT';
-	// Try to be nice. Take a look at the anti spam laws. In most cases, you must
-	// have an unsubscribe. You also cannot be misleading.
-	$subject = "YOUR_SUBJECT";
-	$from = new SendGrid\Email(null, $FROM_EMAIL);
-	$to = new SendGrid\Email(null, $TO_EMAIL);
-	$htmlContent = '';
-	// Create Sendgrid content
-	$content = new SendGrid\Content("text/html",$htmlContent);
-	// Create a mail object
-	$mail = new SendGrid\Mail($from, $subject, $to, $content);
-	
-	$sg = new \SendGrid($API_KEY);
-	$response = $sg->client->mail()->send()->post($mail);
-			
-	if ($response->statusCode() == 202) {
-		// Successfully sent
-		echo 'done';
-	} else {
-		echo $response;
-	}
+  if(isset($_POST))
+  {
+  
+    $FROM_EMAIL = 'Moustafa@psu.edu';
+  	// they dont like when it comes from @gmail, prefers business emails
+  	$TO_EMAIL = 'THE_PERSON_YOUR_WANT_TO_CONTACT';
+  	// Try to be nice. Take a look at the anti spam laws. In most cases, you must
+  	// have an unsubscribe. You also cannot be misleading.
+  	$subject = "YOUR_SUBJECT";
+  	$from = new SendGrid\Email(null, $FROM_EMAIL);
+  	$to = new SendGrid\Email(null, $TO_EMAIL);
+  	$htmlContent = '';
+  	// Create Sendgrid content
+  	$content = new SendGrid\Content("text/html",$htmlContent);
+  	// Create a mail object
+  	$mail = new SendGrid\Mail($from, $subject, $to, $content);
+  	
+  	$sg = new \SendGrid($API_KEY);
+  	$response = $sg->client->mail()->send()->post($mail);
+  			
+  	if ($response->statusCode() == 202) {
+  		// Successfully sent
+  		echo 'done';
+  	} else {
+  		echo $response;
+  	}
+  }
 ?>
