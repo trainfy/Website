afa44a2e2d99cb560e999df0e44aa0e3e98108db
MoustafaElhadary <MoustafaElhadary96@gmail.com>
2019-02-08 00:00:09 +0000

    a

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,9 +1,9 @@
 <?php
 // You need to install the sendgrid client library so run: composer require sendgrid/sendgrid
-	require './lib/sendgrid-php/vendor/autoload.php';
+// 	require '../lib/sendgrid-php/vendor/autoload.php';
+	require("../lib//sendgrid-php/sendgrid-php.php");
 	// contains a variable called: $API_KEY that is the API Key.
 	// You need this API_KEY created on the Sendgrid website.
-	include_once('./credentials.php');
 	
 	$FROM_EMAIL = 'MoustafaElhadary96@gmail.com';
 	// they dont like when it comes from @gmail, prefers business emails
@@ -11,15 +11,16 @@
 	// Try to be nice. Take a look at the anti spam laws. In most cases, you must
 	// have an unsubscribe. You also cannot be misleading.
 	$subject = "YOUR_SUBJECT";
-	$from = new SendGrid\Email(null, $FROM_EMAIL);
-	$to = new SendGrid\Email(null, $TO_EMAIL);
+	$from = new \SendGrid\Email(null, $FROM_EMAIL);
+	$to = new \SendGrid\Email(null, $TO_EMAIL);
 	$htmlContent = '';
 	// Create Sendgrid content
-	$content = new SendGrid\Content("text/html",$htmlContent);
+	$content = new \SendGrid\Content("text/html",$htmlContent);
 	// Create a mail object
-	$mail = new SendGrid\Mail($from, $subject, $to, $content);
+	$mail = new \SendGrid\Mail($from, $subject, $to, $content);
 	
-	$sg = new \SendGrid($API_KEY);
+ $apiKey = getenv('SENDGRID_API_KEY');
+ $sg = new \SendGrid($apiKey);
 	$response = $sg->client->mail()->send()->post($mail);
 			
 	if ($response->statusCode() == 202) {
